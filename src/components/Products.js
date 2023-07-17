e8927f1d0b10ed5a557b7fee94d1a301384a68f5
mmadhesh <mmadhesh@global.sial.com>
2023-07-17 11:38:59 +0530

    map added

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,11 +1,26 @@
 import React from "react";
-import car from "../images/car.jfif";
+import carTata from "../images/car-tata.jfif";
+import carMaruti from "../images/car-maruti.jpeg";
 
 const Products = () => {
+  const items = [
+    { id: 1, name: "Maruti", image: carMaruti },
+    { id: 2, name: "Tata", image: carTata },
+    { id: 3, name: "Maruti", image: "EC" },
+    { id: 4, name: "Maruti", image: "EC" },
+    { id: 5, name: "Maruti", image: "EC" },
+  ];
   return (
-    <>
-      <img src={car} alt={"car"} />
-    </>
+    <ul>
+      {items.map((item) => {
+        return (
+          <li key={item.id}>
+            {item.name}
+            <img src={item.image} alt="car" />
+          </li>
+        );
+      })}
+    </ul>
   );
 };
 
